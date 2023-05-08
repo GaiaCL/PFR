8db12d9da7100a539e8c4bfc1b13cc26f8facbc8
GaiaCL <gaia.coelsch@gmail.com>
2023-05-08 16:27:09 +0200

    Gaia

--- admin/templates/addCategorie.php
+++ admin/templates/addCategorie.php
@@ -1,12 +1,12 @@
 <?php $title = "Ajout Catégorie"; ?>
-<?php  ob_start();?>
-    <h1 style="box-shadow: none;margin-top:2em;">Ajouter une Catégorie</h1>
-    <form method="POST" action="">
-        <?= $msg?>
-        <label>Nom de la Catégorie : </label><input type="text" name="nom">
-        <label>Description de la Catégorie : </label><textarea name="description"></textarea>
-        <input type="submit" name="valider" value="Ajouter">
+<?php ob_start(); ?>
+<h1 style="box-shadow: none;margin-top:2em;">Ajouter une Catégorie</h1>
+<form method="POST" action="">
+    <?= $msg ?>
+    <label>Nom de la Catégorie : </label><input type="text" name="nom">
+    <label>Description de la Catégorie : </label><textarea name="description"></textarea>
+    <input type="submit" name="valider" value="Ajouter">
+</form>
 <?php $content = ob_get_clean();
 require('layoutAdmin.php');
 ?>
-    </form> 
