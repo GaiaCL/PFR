0abda9fc2558b11cdc463577ecb53e3bebc757c2
GaiaCL <gaia.coelsch@gmail.com>
2023-05-10 16:48:49 +0200

    Gaia

--- admin/src/model/modificationCategorie.php
+++ admin/src/model/modificationCategorie.php
@@ -3,25 +3,23 @@ function selectModifCat(){
 if (isset($_GET['id'])){
     $id = intval($_GET['id']);
     $sql = "SELECT*FROM categories WHERE id = :id";
-    $idsStatement = dbConnect()->prepare($sql);
-    $idsStatement->bindValue(':id', $id, PDO::PARAM_INT);
-    $idsStatement->execute();
-    $res = $idsStatement->fetch();
-    $nomCat = $res['name_categorie'];
-    $descriptionCat = $res['description'];
+    $stmt = dbConnect()->prepare($sql);
+    $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+    $res = $stmt->fetchAll();
     if (isset($_POST['nomCat']) && isset($_POST['descCat'])) {
         $nomUpdate = strip_tags($_POST['nomCat']);
         $descriptionUpdate = strip_tags($_POST['descCat']); 
         if(isset($_POST['valider'])){
             $query = 'UPDATE categories SET name_categorie = :name_categorie, description = :description WHERE id = :id';
-            $idsStatement = dbConnect()->prepare($query);
-            $idsStatement->bindValue(':id', $id, PDO::PARAM_INT); 
-            $idsStatement->bindValue(':name_categorie', $nomUpdate, PDO::PARAM_STR);
-            $idsStatement->bindValue(':description', $descriptionUpdate, PDO::PARAM_STR);
-            $idsStatement->execute();
+            $stmt = dbConnect()->prepare($query);
+            $stmt->bindValue(':id', $id, PDO::PARAM_INT); 
+            $stmt->bindValue(':name_categorie', $nomUpdate, PDO::PARAM_STR);
+            $stmt->bindValue(':description', $descriptionUpdate, PDO::PARAM_STR);
+            $stmt->execute();
             header("location:index.php?page=manageCat");
         }
     }
 }
-return [$nomCat,$descriptionCat];
+return $res;
 }
